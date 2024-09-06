ff904df1720f2fa4a60ac3bcfca389b351cf063c
Luke Webb <lukewebb2000@gmail.com>
2024-09-06 16:03:34 +1200

    Restrcture project, added main.rb, fixed required files

--- lib/game_loop.rb
+++ lib/game_loop.rb
@@ -1,25 +1,22 @@
 # frozen_string_literal: true
 
 # require_relative 'player.ruby'
-# require_relative 'board.ruby'
+require_relative 'board'
 
 # Main loop that runs the Tic-Tac-Toe game
 class Gameloop
-  # def initialize
-  #   @board = Board.new
-  # end
+  def initialize
+    @game_board = Board.new
+  end
 
   def run
     puts "Welcome to Tic-Tac-Toe!\nEnter \"1\" to play"
     input = gets
     until input == 'QUIT'
       # Do lots of stuff
+      @game_board.display_board
       puts "Type your option:\n\nor \'QUIT\' to end the game"
       input = gets.chomp
     end
   end
 end
-
-game = Gameloop.new
-
-game.run
---
+++ main.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'lib/game_loop'
+
+game = Gameloop.new
+game.run
