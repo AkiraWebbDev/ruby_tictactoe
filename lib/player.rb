29bfa5148f7b2f66be4612b3d83e84eb24739f74
Luke Webb <lukewebb2000@gmail.com>
2024-09-07 13:59:17 +1200

    Added logic to force 1 char symbols.

--- lib/player.rb
+++ lib/player.rb
@@ -7,12 +7,12 @@ class Player
   def initialize(name, is_bot = false) # rubocop:disable Style/OptionalBooleanParameter
     @name = name
     @is_bot = is_bot
-    if is_bot
-      @symbol = 'X'
-    else
-      puts "Enter the symbol for #{name}"
-      @symbol = gets.chomp
+    chosen_symbol = 'more than 1 character'
+    while chosen_symbol.length > 1
+      puts "Enter the symbol for #{name}\n(Must be 1 character)"
+      chosen_symbol = gets.chomp
     end
+    @symbol = chosen_symbol
   end
 
   # Chooses a random number from the board in okay
